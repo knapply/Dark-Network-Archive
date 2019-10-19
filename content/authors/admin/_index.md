9fb84a781c89ab73e64814d2f387b5452d78b6e3
Brendan <brendan.g.knapp@gmail.com>
2019-10-18 17:41:27 -0700

    init

---
+++ content/authors/admin/_index.md
@@ -0,0 +1,26 @@
+---
+authors:
+- admin
+email: ""
+name: CORE Lab
+organizations:
+- name: Defense Analysis Department, Naval Postgraduate School
+  url: ""
+social:
+- icon: envelope
+  icon_pack: fas
+  link: '#contact'
+- icon: twitter
+  icon_pack: fab
+  link: https://twitter.com/NPS_CORELab
+- icon: github
+  icon_pack: fab
+  link: https://github.com/NPSCORELAB
+superuser: true
+---
+
+Researchers have increasingly turned to social network analysis (SNA) for gaining greater understanding of “dark networks,” that is, covert and illegal networks, such as terrorist groups and criminal gangs, which seek to conceal their activities from authorities. 
+
+Unfortunately, because data on dark networks is somewhat limited and often anecdotal, it is difficult to evaluate the various theories that researchers, academics, and subject matter experts have developed about what makes some dark networks successful and what the best intervention strategies are for disrupting them. 
+
+The only way to resolve these differences is for analysts to compare and contrast dark networks across time and space; only then can they identify those characteristics (e.g., level of centralization, cohesion) that tend to be associated with successful dark networks and those associated with unsuccessful ones. That is where the CORE Lab’s Dark Network Archive (CORE DNA) comes in. 
---
+++ network-page-build/build_network_pages.R
@@ -0,0 +1,60 @@
+build_network_pages <- function(proto_net_names) {
+  on.exit(options(warn = 0))
+  options(warn = 2) # FAIL ON ANY WARNINGS
+  
+  stopifnot(length(proto_net_names) && is.character(proto_net_names))
+  
+  valid_names <- COREnets::list_data_sources()
+  
+  if (!all(proto_net_names %in% valid_names)) {
+    invalid_names <- setdiff(proto_net_names, valid_names)
+    stop("The following `net_names` are invalid:",
+         paste0("\n\t- ", invalid_names),
+         call. = FALSE)
+  }
+  
+  target_nets <- intersect(proto_net_names, valid_names)
+  if (length(target_nets) == 0L) {
+    stop("No valid nets found.", call. = FALSE)
+  }
+  
+  for (i in seq_along(target_nets)) {
+    out_dir <- paste0("content/post/", target_nets[[i]])
+    if (!dir.exists(out_dir)) {
+      dir.create(out_dir)
+    }
+    out_path <- paste0(out_dir, "/", "index.Rmd")
+    
+    message("Rendering:\t", target_nets[[i]])
+    
+    this_net <- COREnets::get_data(target_nets[[i]])
+
+    rmd <- readr::read_file(
+      "network-page-build/network-page-template.Rmd"
+    )
+    
+    .gsub <- function(string, pattern, replacement) {
+      gsub(pattern, replacement, string, fixed = TRUE)
+    }
+    
+    rmd <- .gsub(rmd, "{{{TITLE}}}", this_net$reference$title)
+    rmd <- .gsub(rmd, "{{{SUMMARY}}}", this_net$reference$abstract[[1L]])
+    rmd <- .gsub(rmd, "{{{PROTO_NET_NAME}}}", this_net$reference$name)
+    rmd <- .gsub(
+      rmd, '"{{{TAGS}}}"', 
+      paste0("\n- ", this_net$reference$tags, collapse = "")
+    )
+    
+    readr::write_file(rmd, out_path)
+  }
+}
+
+build_network_pages(
+  c("drugnet", "anabaptists", "cocaine_smuggling_acero",
+    "cocaine_smuggling_jake", "cocaine_smuggling_juanes",
+    "cocaine_smuggling_mambo", "montreal_street_gangs",
+    "november17", "siren", "harry_potter_death_eaters",
+    "harry_potter_dumbledores_army")
+)
+
+
